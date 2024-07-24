ad818054fda77da0b79a99c8de063d07521a9123
ef <e.fuchs@cluetec.de>
2024-07-24 18:32:32 +0200

    Initial Commit

---
+++ GambleGame/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  GambleGame
+//
+//  Created by Edgar Fuchs on 24.07.24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GambleGame/GambleGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GambleGameApp.swift
+//  GambleGame
+//
+//  Created by Edgar Fuchs on 24.07.24.
+//
+
+import SwiftUI
+
+@main
+struct GambleGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
